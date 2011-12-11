24e2ec18380c423218e5e203b4ee5bbfb1c96095
Karl Voelker <ktvoelker@gmail.com>
2011-12-10 18:47:16 -0800

    Changed AST to have embedded location information

--- AST.hs
+++ AST.hs
@@ -7,9 +7,17 @@ import Lexer
 
 type P = Parsec [(Token, SourcePos, Maybe String)] ()
 
+data L a = L
+  { loc :: SourcePos
+  , val :: a
+  } deriving (Eq, Show)
+
+vals :: (Functor f) => f (L a) -> f a
+vals = fmap val
+
 data SimpleName = SimpleName String deriving (Eq, Show)
 
-data Name = Name [SimpleName] deriving (Eq, Show)
+data Name = Name [L SimpleName] deriving (Eq, Show)
 
 data Param =
     TypeParam SimpleName
@@ -19,56 +27,56 @@ data Param =
 type Type = Expr
 
 data Expr =
-    EApp Expr Args
+    EApp (L Expr) Args
   | EInt Integer
   | EFloat Double
   | EChar Char
   | EString String
   | EName Name
-  | EArray [Expr]
-  | ENew Name Args
-  | EFnValue Params Type FnBody
-  | EFnType Params Type
-  | ELoopInfo LoopInfo SimpleName
+  | EArray [L Expr]
+  | ENew (L Name) Args
+  | EFnValue Params (L Type) (L FnBody)
+  | EFnType Params (L Type)
+  | ELoopInfo LoopInfo (L SimpleName)
   deriving (Eq, Show)
 
-data Args = Args [Expr] [Expr] deriving (Eq, Show)
+data Args = Args [L Expr] [L Expr] deriving (Eq, Show)
 
 data Params =
   Params
-  { pStatics  :: [Param]
-  , pDynamics :: [Param]
+  { pStatics  :: [L Param]
+  , pDynamics :: [L Param]
   } deriving (Eq, Show)
 
 data LoopInfo = LFirst | LLast | LPos | LCount deriving (Eq, Show)
 
 type FnBody = Either Block Expr
 
-type Block = [Stmt]
+type Block = [L Stmt]
 
-type LoopLabel = Maybe SimpleName
+type LoopLabel = Maybe (L SimpleName)
 
 data Stmt =
     SExpr Expr
-  | SDef Bool SimpleName Expr
-  | SIf (Maybe Expr) Expr Block [(Expr, Block)] (Maybe Block)
-  | SWhile LoopLabel Expr Block (Maybe Block)
-  | SFor LoopLabel SimpleName Expr Block (Maybe Block)
+  | SDef Bool (L SimpleName) (L Expr)
+  | SIf (Maybe (L Expr)) (L Expr) Block [(L Expr, Block)] (Maybe Block)
+  | SWhile LoopLabel (L Expr) Block (Maybe Block)
+  | SFor LoopLabel (L SimpleName) (L Expr) Block (Maybe Block)
   | SNext LoopLabel
   | SDone LoopLabel
-  | SReturn (Maybe Expr)
-  | SDecide [Expr] [([Maybe Expr], Block)]
+  | SReturn (Maybe (L Expr))
+  | SDecide [L Expr] [([Maybe (L Expr)], Block)]
   | SDo Block
   | SEmpty
   deriving (Eq, Show)
 
-data Namespace = Namespace Name [Import] [NsDef] deriving (Eq, Show)
+data Namespace = Namespace (L Name) [L Import] [L NsDef] deriving (Eq, Show)
 
 data Kind = Struct | Union | Enum | Mask deriving (Eq, Show)
 
 data StructElem =
   StructElem
-  { seName :: SimpleName
+  { seName :: L SimpleName
   , seType :: (Maybe Type)
   , seInit :: (Maybe Expr)
   } deriving (Eq, Show)
@@ -77,26 +85,26 @@ data NsDef =
     NsValueDef
     { vdExport :: Bool
     , vdGlobal :: Bool
-    , vdName   :: SimpleName
-    , vdValue  :: Expr
+    , vdName   :: L SimpleName
+    , vdValue  :: L Expr
     }
   | NsTypeDef
     { tdKind   :: Kind
-    , tdArgs   :: [Expr]
-    , tdName   :: SimpleName
+    , tdArgs   :: [L Expr]
+    , tdName   :: L SimpleName
     , tdParams :: Params
     , tdElems  :: [StructElem]
     }
   | NsTypeAlias
-    { taName   :: SimpleName
-    , taParams :: [Param]
-    , taType   :: Type
+    { taName   :: L SimpleName
+    , taParams :: [L Param]
+    , taType   :: L Type
     }
   deriving (Eq, Show)
 
 data Import =
   Import
-  { iFrom  :: Name
-  , iNames :: (Maybe [SimpleName])
+  { iFrom  :: L Name
+  , iNames :: (Maybe [L SimpleName])
   } deriving (Eq, Show)
 
